602de2ce71a1c5b6fd8774989934314ed0147c8a
thea chau <90798238+theach0711@users.noreply.github.com>
2022-02-15 10:11:18 -0700

    Add files via upload

---
+++ src/main/resources/sampleData.sql
@@ -0,0 +1,29 @@
+INSERT INTO `_PT ID` ( Lastname, Firstname, Sex, Birthday, ULI, TG, Location) 
+VALUES ('Dervis', 'John', 'M','24-FEB-1972', '732917261', 'GI', 'RRDC');
+
+INSERT INTO `_PT ID` ( Lastname, Firstname, Sex, Birthday, ULI, TG, Location) 
+VALUES ('Finch', 'Adam', 'M', '1998-05-05', '940733349', 'HN' , 'RRDC');
+
+INSERT INTO `_PT ID` ( Lastname, Firstname, Sex, Birthday, ULI, TG, Location) 
+VALUES ('Huey', 'Linda', 'F', '1977-07-17', '673972108', 'Lung', 'RRDC' );
+
+INSERT INTO `_PT ID` ( Lastname, Firstname, Sex, Birthday, ULI, TG, Location) 
+VALUES ('Dosi', 'Serena', 'F', '1980-12-31', '182560549', 'HPB', 'Inpatient');
+
+INSERT INTO `_PT ID` ( Lastname, Firstname, Sex, Birthday, ULI, TG, Location) 
+VALUES ('Manabdha', 'Abraham', 'M', '2000-02-28', '384266529', 'GU', 'TBCC' );
+
+INSERT INTO `_PT ID` ( Lastname, Firstname, Sex, Birthday, ULI, TG, Location) 
+VALUES ('Martzen', 'Jane', 'F', '1997-08-01', '181641135', 'Sarcoma', 'Inpatient' );
+
+INSERT INTO `_PT ID` ( Lastname, Firstname, Sex, Birthday, ULI, TG, Location) 
+VALUES ('Iimura','Joe', 'F', '1978-03-11', '841180517', 'Breast', 'HXH' );
+
+INSERT INTO `_PT ID` ( Lastname, Firstname, Sex, Birthday, ULI, TG, Location)  
+VALUES ('Brigandi', 'Sam', 'M', '1976-09-26', '287809466', 'HPB', 'HXH');
+
+INSERT INTO `_PT ID` ( Lastname, Firstname, Sex, Birthday, ULI, TG, Location) 
+VALUES ('Grassini', 'Jay', 'M', '1969-04-08', '557194155', 'GU', 'HXH');
+
+INSERT INTO `_PT ID` ( Lastname, Firstname, Sex, Birthday, ULI, TG, Location) 
+VALUES ('Haskell', 'Sarah', 'F', '1999-11-02', '857376351', 'HN', 'RRDC');
