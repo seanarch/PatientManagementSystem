d8bece0df597e334bdc8c2b9beeeb107e4fae943
Sean <zhuqingyun@outlook.com>
2022-02-15 17:37:59 -0700

    Add search firstname

--- src/main/frontend/src/pages/previsit.js
+++ src/main/frontend/src/pages/previsit.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Previsit_search from './Previsit/Previsit_search'
+import PatientData from "../data/patientsdata.json"
 
 const Previsit = () => {
     return (
-        <div style={{
+        <div className='previsit' style={{
             display: 'flex', justifyContent:
                 'center', alignItems: 'center', height: '37vh'
         }}>
 
-            <Previsit_search />
+            <Previsit_search placeholder="Enter a Patient Name" data={PatientData} />
         </div>
     )
 }
