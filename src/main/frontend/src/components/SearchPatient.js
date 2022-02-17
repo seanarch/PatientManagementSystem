c4131f33ce0d8559e6f9105f3ab663e44ade598d
Sean <zhuqingyun@outlook.com>
2022-02-17 01:38:44 -0700

    Added SearchPatient.js work with api

---
+++ src/main/frontend/src/components/SearchPatient.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Card, Input } from 'semantic-ui-react'
+export default function SearchPatient() {
+    const [APIData, setAPIData] = useState([])
+    const [filteredResults, setFilteredResults] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
+
+    // Test performed on the following json format api
+
+    // useEffect(() => {
+    //     axios.get(`https://jsonplaceholder.typicode.com/users`)
+    //         .then((response) => {
+    //             setAPIData(response.data);
+    //         })
+    // }, [])
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/api/patient/`)
+            .then((response) => {
+                setAPIData(response.data);
+            })
+    }, [])
+
+    const searchItems = (searchValue) => {
+        setSearchInput(searchValue)
+        if (searchInput !== '') {
+            const filteredData = APIData.filter((item) => {
+                return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
+            })
+            setFilteredResults(filteredData)
+        }
+        else {
+            setFilteredResults(APIData)
+        }
+    }
+
+    return (
+        <div style={{ padding: 20 }}>
+            <Input icon='search'
+                placeholder='Search by firstname...'
+                onChange={(e) => searchItems(e.target.value)}
+            />
+            <Card.Group itemsPerRow={3} style={{ marginTop: 20 }}>
+                {searchInput.length > 1 ? (
+                    filteredResults.map((item) => {
+                        return (
+                            <Card>
+                                <Card.Content>
+                                    <Card.Header>{item.Firstname}</Card.Header>
+
+                                </Card.Content>
+                            </Card>
+                        )
+                    })
+                ) : (
+                    APIData.map((item) => {
+                        return (
+                            <Card>
+                                <Card.Content>
+                                    <Card.Header>{item.Firstname}</Card.Header>
+
+                                </Card.Content>
+                            </Card>
+                        )
+                    })
+                )}
+            </Card.Group>
+        </div>
+    )
+}
