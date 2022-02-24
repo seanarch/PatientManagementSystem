f8f85cef102a726b87ae537717200b4054964195
Sean <zhuqingyun@outlook.com>
2022-02-24 11:34:12 -0700

    Add required components on previsit page

--- src/main/frontend/src/pages/Previsit/Previsit_patientlist.js
+++ src/main/frontend/src/pages/Previsit/Previsit_patientlist.js
@@ -20,8 +20,30 @@ const Previsit_patientlist = () => {
     }, [])
 
     return (
-        <div className="">
+        <div className="container">
+            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
             <h3>List of patient information</h3>
+            <hr />
+            <div>
+                <h3>Text input component</h3>
+                <textarea name='comment' rows="5" cols="30"></textarea>
+                <br></br>
+                <h3>Date input component</h3>
+                <input type="date" id="datedi" />
+                <br></br>
+                <h3>Dropdown menu component</h3>
+                <select>
+                    <option>1</option>
+                    <option>2</option>
+                </select>
+                <br></br>
+                <h3>Checkbox component</h3>
+                <input type="checkbox" id="" name="" />
+                <br></br>
+
+
+            </div>
 
 
         </div>
