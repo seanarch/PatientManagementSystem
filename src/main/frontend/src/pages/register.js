6ce098231e3d031ca295afd645cb827054988792
Alix Shuk Han Chau <86642948+Alixchau@users.noreply.github.com>
2022-03-04 18:00:44 -0700

    1. set up register page and other page with material/reactstrap UI applied 2. fetch input and mapped them with data objects
    remarks: need to run below for ui package
    npm install @mui/material @emotion/react @emotion/styled
    AND
    npm install reactstrap

--- src/main/frontend/src/pages/register.js
+++ src/main/frontend/src/pages/register.js
@@ -1,16 +1,70 @@
 import React from 'react'
-import Patients from './Patients/Patients'
+import Patients from './Patients/Patients';
+import { useFormik } from 'formik';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
+/* npm install reactstrap
+ */
 const Register = () => {
-    return (
-        <div style={{
-            display: 'flex', justifyContent:
-                'center', alignItems: 'center', height: '50vh', marginLeft: '37vh'
-        }}>
 
-            <Patients />
+    const registerForm = useFormik({
+        initialValues: {
+            NewPatientConsult: {
+                Date: ""
+            },
+            Death: {
+                Date: "",
+                Detail: "",
+            }
+        },
+        onSubmit: (values) => {
+            console.log(values)
+            //once submit, provide value for back end
+        }
+    });
+
+    return (
+        <div className='container' style={{
+            display: 'flex', justifyContent:
+                'center', alignItems: 'center', height: '50vh'
+        }}>
+            <Form style={{ width: '70vw' }} onSubmit={registerForm.handleSubmit}>
+
+                <FormGroup>
+                    <Label>NP</Label>
+                    <Input
+                        required
+                        name="NewPatientConsult.Date"
+                        type="date"
+                        onChange={registerForm.handleChange}
+                        value={registerForm.values.NewPatientConsult.Date}
+                    />
+
+                </FormGroup>
+
+                <FormGroup>
+                    <Label>Death</Label>
+                    <Input
+                        name="Death.Date"
+                        type="date"
+                        onChange={registerForm.handleChange}
+                        value={registerForm.values.Death.Date}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Input
+                        name="Death.Detail"
+                        type="text"
+                        onChange={registerForm.handleChange}
+                        value={registerForm.values.Death.Detail}
+                    />
+                </FormGroup>
+                <Button color='primary' type="submit">Submit</Button>
+            </Form>
+
+            {/*   <Patients /> */}
 
         </div>
     )
-}
-export default Register
+};
+export default Register;
