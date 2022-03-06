7b3ea60ff075e31521cef4eba2a94ff41c98f11c
Alix Shuk Han Chau <86642948+Alixchau@users.noreply.github.com>
2022-03-06 13:41:15 -0700

    Commit DatePicker and TextField component

---
+++ src/main/frontend/src/pages/Register/RegisterForm.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import { Button } from 'reactstrap';
+import { Container, Grid, InputLabel, Select, MenuItem, FormControl } from '@material-ui/core';
+import TextField from '../../components/TextField/TextFeild';
+import DatePicker from '../../components/Date/DatePicker';
+
+
+const INITIAL_VALUES = {
+    PatientInformation: {
+        Lastname: "",
+        Firstname: "",
+        Sex: "",
+        Birthday: "",
+        ULI: "",
+        TG: "",
+        Location: ""
+    },
+    NewPatientConsult: {
+        Date: ""
+    }
+}
+
+const RegisterForm = () => {
+    return (
+
+        <Container maxWidth="md">
+            <div className='container' style={{
+                display: 'flex', justifyContent:
+                    'center', alignItems: 'center', marginTop: '50px'
+            }}>
+                <Formik initialValues={{ ...INITIAL_VALUES }} onSubmit={values => {
+                    console.log(values)//once submit, provide value for back end
+                }}
+>
+                    {props => (
+                        <Form>
+                            <h3>Create New Patient</h3>
+                            <Grid container spacing={3} width={'70vw'}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        label="Lastname"
+                                        name="PatientInformation.Lastname"
+                                        fullWidth
+                                    />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            label="Firstname"
+                                            name="PatientInformation.Firstname"
+                                        />
+                                    </Grid>
+
+                                    <Grid item xs={12} >
+                                    <FormControl fullWidth>
+                                        <InputLabel id="demo-simple-select">Sex</InputLabel>
+                                        <Select
+                                            label="Sex"
+                                            name="PatientInformation.Sex"
+                                            id="demo-simple-select"
+                                            value={props.values.PatientInformation.Sex}
+                                            onChange={props.handleChange}
+                                            labelId="demo-simple-select-label"
+      
+                                        >
+                                            <MenuItem value={"M"}>Male</MenuItem>
+                                            <MenuItem value={"F"}>Female</MenuItem>
+
+                                        </Select>
+                                        </FormControl>
+                                    </Grid>
+
+                                    <Grid item xs={12}>
+                                        <DatePicker
+                                            fullWidth
+                                            name="PatientInformation.Birthday"
+                                            label="Birthday"
+                                        />
+                                    </Grid>
+
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            label="ULI"
+                                            name="PatientInformation.ULI"
+                                            required
+                                        />
+                                    </Grid>
+
+
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            label="TG"
+                                            name="PatientInformation.TG"
+                                        />
+                                    </Grid>
+
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            label="Location"
+                                            name="PatientInformation.Location"
+                                        />
+                                    </Grid>
+
+                                    <Grid item xs={12}>
+                                        <DatePicker
+                                            fullWidth
+                                            name="NewPatientConsult.Date"
+                                            label="New Patient Date"
+                                        />
+                                    </Grid>
+
+                                    <Grid item xs={12}>
+                                        <Button color='primary' type="submit">Create</Button>
+                                    </Grid>
+                                </Grid>
+                            
+                        </Form>
+                    )}
+                </Formik>
+            </div>
+        </Container>
+    )
+}
+
+export default RegisterForm;
---
+++ src/main/frontend/src/pages/Register/RegisterPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Patients from '../Patients/Patients';
+import RegisterForm from './RegisterForm';
+import DeathForm from './DeathForm';
+/* npm install reactstrap
+ */
+const Register = () => (
+    <div>
+    <RegisterForm />
+    <DeathForm />
+    </div>
+);
+
+export default Register;
