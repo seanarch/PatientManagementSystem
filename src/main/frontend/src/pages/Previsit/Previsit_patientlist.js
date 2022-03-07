2586809e9cbe30fcbcb0c1f1c8d92ebbdee32d23
Sean <seanzhu@Seans-Mac-mini.local>
2022-03-06 20:44:50 -0700

    Previsit page with date, dropdown, textarea and validation

--- src/main/frontend/src/pages/Previsit/Previsit_patientlist.js
+++ src/main/frontend/src/pages/Previsit/Previsit_patientlist.js
@@ -2,119 +2,113 @@ import React from "react";
 import patientService from "../../services/patient.service";
 import * as Yup from "yup";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+const initialValues = {
+    datedi: "2021-02-17",
+    ctChest: "No",
+    comments: "Some comments here",
+    date1: "2019-11-21",
+    bili: "19-Nov-21",
+    alb: "19-Nov-21",
+    ptInr: "19-Nov-21",
+    ascites: "1",
+    he: "Yes",
+    cpScore: "1",
+    childPugh: "1",
+    date2: "",
+    fvc: "",
+    fvcPercent: "",
+    fev1: "",
+    fev1Percent: "",
+    ratio: ""
+}
+
+const onSubmit = values => {
+    console.log('Form submission data', values)
+}
+
+const validationSchema = Yup.object({
+    comments: Yup.string().required('Required field'), 
+    date1: Yup.date()
+    .min(new Date('01-01-2019'))
+    .max(new Date())
+    .required(),
+    date2: Yup.date()
+        .min(new Date('01-01-2019'))
+        .max(new Date())
+        .required(),
+    fvc: Yup.string().required('Required field')
+    .matches(/^[0-9]+$/, "Must be only digits")
+    .min(2, 'Must be exactly 2 digits')
+    .max(2, 'Must be exactly 2 digits'),
+    he: Yup.string()
+    .oneOf(['Yes', 'No'])
+    .required('Please either select YES or No'),
+    datedi: Yup.date()
+    .required('Date is required'),
+    ctChest: Yup.string()
+    .ensure()
+    .oneOf(['Yes', 'No'])
+    .required('Please either select YES or No'),
+    
+})
 
 const Previsit_patientlist = () => {
-    // Use traditional way instead of formik
-
-    // const [patients, setPatients] = useState([]);
-    // const [comments, setcomments] = useState('');
-    // const [textinput, setTextinput] = useState('');
-    // const [date, setDate] = useState('');
-    // const [dropdown, setDropdown] = useState('');
-    // const [checkbox, setCheckbox] = useState('');
-
-
-
-    // useEffect(() => {
-    //     patientService.getAll()
-    //         .then(response => {
-    //             console.log('Printing patients data', response.data);
-    //             setPatients(response.data);
-    //         })
-    //         .catch(error => {
-    //             console.log('Something wrong', error);
-    //         })
-
-    // }, [])
-
-    // console.log(comments);
-    // console.log(textinput);
-    // console.log(date);
-    // console.log(dropdown);
-    // console.log(checkbox);
-
-
-    const formik = useFormik({
-
-        initialValues: {
-            comments: "",
-            textinput: "",
-            datedi: "",
-            dropdown: "",
-            checkbox: "",
-        },
-        // added validation
-        validationSchema: Yup.object({
-            comments: Yup.string().max(15, "Must be 15 characters or less").required("Required"),
-        }),
-        onSubmit: (values) => {
-            console.log(values);
-        },
-    });
-
-    console.log(formik.errors);
-
-
+    
     return (
-        <div className="container">
-            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-            <h3>List of patient information</h3>
-            <hr />
-            <form onSubmit={formik.handleSubmit}>
-
-                <div className="input-container">
-                    <h3>Textarea input component</h3>
-                    <textarea
-                        id="comments"
-                        name="comments"
-                        rows="5" cols="30"
-                        onChange={formik.handleChange}
-                        value={formik.values.comments}></textarea>
-                    {/* added error message */}
-                    {formik.errors.comments ? <p>{formik.errors.comments}</p> : null}
-                    <hr />
+        
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}>
+            
+            <Form>
+                <br></br><br></br><br></br><br></br>
+                <br></br><br></br><br></br><br></br>
+                <div className="form-control">
+                    <label previsitForm='datedi'>Datedi</label>
                     <br></br>
-                    <h3>Text input component</h3>
-                    <input type="text"
-                        name="textinput"
-                        onChange={formik.handleChange}
-                        value={formik.values.textinput} />
-                    <hr />
-                    <br></br>
-                    <h3>Date input component</h3>
-                    <input type="date" name="datedi"
-                        onChange={formik.handleChange}
-                        value={formik.values.datedi} />
-                    <hr />
-                    <br></br>
-                    <h3>Dropdown menu component</h3>
-                    <select name="dropdown"
-                        onChange={formik.handleChange}
-                        value={formik.values.dropdown}>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                    </select>
-                    <hr />
-                    <br></br>
-                    <h3>Checkbox component</h3>
-                    <input type="checkbox" id="checkbox" name="checkbox"
-                        onChange={formik.handleChange}
-                        value={formik.values.checkbox} />
-                    <hr />
-                    <br></br>
-                    <div>
-
-                        <button type="submit" className="btn btn-primary">Save changes</button>
-                    </div>
+                    <Field type="date" id="datedi" name="datedi" />
+                    <ErrorMessage name="datedi" />
                 </div>
+                <br></br><br></br>
+                <div className="form-control">
+                <label previsitForm='ctChest'>CT Chest</label>
+                <br></br>
+                <Field as="select" id="ctChest" name="ctChest">
+                    <option value="" label="Select an option" />
+                    <option value="Yes" label="Yes" />
+                    <option value="No" label="No" />
+                </Field>
+                <ErrorMessage name="ctChest" />
+                </div>
+                <br></br><br></br>
+                <div className="form-control">
+                <label previsitForm='comments'>Comments</label>
+                <br></br>
+                <Field type="textarea" id="comments" name="comments" />
+                <ErrorMessage name="comments" />
+                </div>
+                <br></br><br></br>
+                <div className="form-control">
+                    <label previsitForm='date1'>Date1</label>
+                    <br></br>
+                    <Field type="date" id="date1" name="date1" />
+                    <ErrorMessage name="date1" />
+                </div>
+                <br></br><br></br>
+                <div className="form-control">
+                    <label previsitForm='fvc'>FVC</label>
+                    <br></br>
+                    <Field type="fvc" id="fvc" name="fvc" />
+                    <ErrorMessage name="fvc" />
+                </div>
+            </Form>
 
-            </form>
 
 
-        </div>
+        </Formik>
 
     )
 
