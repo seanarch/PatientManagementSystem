49f362ea6a3d2ef2fdb9488349b492c4283d6773
Sean <seanzhu@Seans-Mac-mini.local>
2022-03-16 20:26:17 -0600

    Add symptoms in visit page

--- src/main/frontend/src/pages/Visit/Fhxdetail.js
+++ src/main/frontend/src/pages/Visit/Fhxdetail.js
@@ -8,17 +8,17 @@ import axios from "axios";
  
 const INITIAL_VALUES = {
     FhxInfo: {
-	FhxDetail: "String",
-	Smoking: 100,
-	SmokingQuit: "3 years",
-	Unattended: false,
-    SocialSup: false,
-    Language: false,
-    $Stress: true,
-    PsychStress: true,
-    ETOHweek: true,
-    Details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    },
+        FhxDetail: "String",
+        Smoking: 100,
+        SmokingQuit: "3 years",
+        Unattended: false,
+        SocialSup: false,
+        Language: false,
+        $Stress: true,
+        PsychStress: true,
+        ETOHweek: true,
+        Details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        },
 }
 
 const Fhxdetail = () => {
