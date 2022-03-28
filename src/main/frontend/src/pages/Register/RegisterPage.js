defd50d0b666008b2bd31b03e474a68d5b42f1fc
bellaegenti1 <77996818+bellaegenti1@users.noreply.github.com>
2022-03-28 14:56:33 -0600

    collapsible trigger added

--- src/main/frontend/src/pages/Register/RegisterPage.js
+++ src/main/frontend/src/pages/Register/RegisterPage.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Patients from '../Patients/Patients';
 import RegisterForm from './RegisterForm';
 import DeathForm from './DeathForm';
+import SearchForm from './SearchForm';
 /* npm install reactstrap
  */
 const Register = () => (
     <div>
-    <RegisterForm />
-    <DeathForm />
+        <SearchForm />
+        <RegisterForm />
+        <DeathForm />
     </div>
 );
 
