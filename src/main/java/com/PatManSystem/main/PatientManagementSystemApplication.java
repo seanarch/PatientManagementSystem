65326bdbda3db8945b168b35ca262be0fad3fee5
Connor <connor.j.meakin@gmail.com>
2022-03-27 20:46:03 -0600

    Exam Service Class init

--- src/main/java/com/PatManSystem/main/PatientManagementSystemApplication.java
+++ src/main/java/com/PatManSystem/main/PatientManagementSystemApplication.java
@@ -2,8 +2,15 @@ package com.PatManSystem.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.PatManSystem.main.Controllers",
+		"com.PatManSystem.main.Services",
+		"com.PatManSystem.main.DTO",
+		"com.PatManSystem.main.Mapper",
+		"com.PatManSystem.main.Repository",
+		"com.PatManSystem.main.Models"})
 public class PatientManagementSystemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/PatManSystem/main/Services/ExamService.java
+++ src/main/java/com/PatManSystem/main/Services/ExamService.java
@@ -1,26 +1,53 @@
 package com.PatManSystem.main.Services;
 
 import com.PatManSystem.main.DTO.ExamDTO;
+import com.PatManSystem.main.DTO.PatientinformationDTO;
 import com.PatManSystem.main.Mapper.ExamMapperImpl;
+import com.PatManSystem.main.Mapper.PatientinformationMapperImpl;
+import com.PatManSystem.main.Models.Exam;
+import com.PatManSystem.main.Models.Patientinformation;
 import com.PatManSystem.main.Repository.ExamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class ExamService {
 
+    private final ExamRepository examRepository;
+
     @Autowired
-    private ExamRepository examRepository;
+    public ExamService(ExamRepository examRepository){
+        this.examRepository = examRepository;
+    }
+
 
     public List<ExamDTO> getExams(){
        return examRepository.findAll()
                .stream()
-               .map(exam -> {
-                   return new ExamMapperImpl().examToExamDTO(exam);
-               })
+               .map(exam -> new ExamMapperImpl().examToExamDTO(exam))
                .collect(Collectors.toList());
+    }
+
+    public ExamDTO getExam(Long id){
+        Exam getExam = examRepository.findExamById(id);
+        if(getExam == null) //check if the requested patient exists, if not; throw not found exception
+            throw new IllegalStateException("Exam identified by ID "+id+" was not found.");
+
+        return new ExamMapperImpl().examToExamDTO(getExam);
+
+    }
+    public List<ExamDTO> getExamByULI(Long ULI){
+        List<Exam> getExams = examRepository.findExamByULI(ULI);
+        if(getExams == null) //check if the requested patient exists, if not; throw not found exception
+            throw new IllegalStateException("Exam identified by ULI "+ULI+" was not found.");
+
+        return getExams.stream()
+                .map(exam -> new ExamMapperImpl().examToExamDTO(exam))
+                .collect(Collectors.toList());
+
     }
 }
