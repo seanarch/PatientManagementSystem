97c0cba88d68825af53a4a23a04218d7f5185d31
Alix Shuk Han Chau <86642948+Alixchau@users.noreply.github.com>
2022-03-30 23:41:43 -0600

    Complete ManagementController and ManagementService

--- src/main/java/com/PatManSystem/main/Repository/ManagementRepository.java
+++ src/main/java/com/PatManSystem/main/Repository/ManagementRepository.java
@@ -1,9 +1,15 @@
 package com.PatManSystem.main.Repository;
 
+
 import com.PatManSystem.main.Models.Management;
+import com.PatManSystem.main.Models.Patientinformation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ManagementRepository extends JpaRepository<Management, Integer> {
+public interface ManagementRepository extends JpaRepository<Management, Long> {
+    Management findManagementById(Long Id);
+    List<Management> findManagementByUli(Long uli);
 }
