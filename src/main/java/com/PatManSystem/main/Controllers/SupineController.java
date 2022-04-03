e55f83dd906d90ddfe9f95a8b0eb3e59048ef350
Connor <connor.j.meakin@gmail.com>
2022-04-03 15:42:05 -0600

    SupineController and SupineService Added

---
+++ src/main/java/com/PatManSystem/main/Controllers/SupineController.java
@@ -0,0 +1,50 @@
+package com.PatManSystem.main.Controllers;
+
+import com.PatManSystem.main.Models.Supine;
+import com.PatManSystem.main.Services.SupineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/supine")
+@CrossOrigin("*")
+public class SupineController {
+
+    private final SupineService supineService;
+
+    @Autowired
+    public SupineController(SupineService supineService) {
+        this.supineService = supineService;
+    }
+
+    @GetMapping(path = "/all")
+    public List<Supine> getSupine() {return supineService.getSupines();
+    }
+
+    @GetMapping(path ="/id={id}")
+    public Supine getSupine(@PathVariable("id") Integer id){
+        return supineService.getSupine(id);
+    }
+
+    @PostMapping(path = "/new")
+    public String registerNewSupine(@RequestBody Supine supine){
+        supineService.newSupine(supine);
+        return "NEW: Supine identified by ID "+supine.getId()+" successfully added.";
+    }
+
+    @DeleteMapping(path = "/delete/id={id}")
+    public String deleteSupine(@PathVariable("id") Integer id){
+        supineService.deleteSupine(id);
+        return "DELETE: Supine identified by ID "+id+" successfully deleted.";
+    }
+
+    @PostMapping(path = "/update")
+    public String updateSupine(@RequestBody Supine supine){
+
+        supineService.updateSupine(supine);
+        return "UPDATE: Supine identified by ID "+supine.getId()+" successfully updated.";
+
+    }
+}
