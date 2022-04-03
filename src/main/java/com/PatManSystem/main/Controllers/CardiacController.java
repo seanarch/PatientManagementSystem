d5c6e4346166ac4b7d34a43837eb6c4504ece330
Connor <connor.j.meakin@gmail.com>
2022-04-03 15:07:48 -0600

    CardiacController and CardiacService Added

---
+++ src/main/java/com/PatManSystem/main/Controllers/CardiacController.java
@@ -0,0 +1,50 @@
+package com.PatManSystem.main.Controllers;
+
+import com.PatManSystem.main.Models.Cardiac;
+import com.PatManSystem.main.Services.CardiacService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/cardiac")
+@CrossOrigin("*")
+public class CardiacController {
+
+    private final CardiacService cardiacService;
+
+    @Autowired
+    public CardiacController(CardiacService cardiacService) {
+        this.cardiacService = cardiacService;
+    }
+
+    @GetMapping(path = "/all")
+    public List<Cardiac> getCardiac() {return cardiacService.getCardiacs();
+    }
+
+    @GetMapping(path ="/id={id}")
+    public Cardiac getCardiac(@PathVariable("id") Integer id){
+        return cardiacService.getCardiac(id);
+    }
+
+    @PostMapping(path = "/new")
+    public String registerNewCardiac(@RequestBody Cardiac cardiac){
+        cardiacService.newCardiac(cardiac);
+        return "NEW: Cardiac identified by ID "+cardiac.getId()+" successfully added.";
+    }
+
+    @DeleteMapping(path = "/delete/id={id}")
+    public String deleteCardiac(@PathVariable("id") Integer id){
+        cardiacService.deleteCardiac(id);
+        return "DELETE: Cardiac identified by ID "+id+" successfully deleted.";
+    }
+
+    @PostMapping(path = "/update")
+    public String updateCardiac(@RequestBody Cardiac cardiac){
+
+        cardiacService.updateCardiac(cardiac);
+        return "UPDATE: Cardiac identified by ID "+cardiac.getId()+" successfully updated.";
+
+    }
+}
