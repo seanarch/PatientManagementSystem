f20c29afc7c43b4daa42b7e3c45479978a6c0f54
bellaegenti1 <77996818+bellaegenti1@users.noreply.github.com>
2022-04-04 16:59:52 -0600

    search bar styling

--- src/main/frontend/src/components/Navbar/NavbarElements.js
+++ src/main/frontend/src/components/Navbar/NavbarElements.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 export const Nav = styled.nav`
   background: #474747;
-  height: 80px;
+  height: 100px;
   display: flex;
   justify-content: space-between;
   padding: 0.5rem calc((100vw - 1000px) / 2);
--- src/main/frontend/src/pages/visit.js
+++ src/main/frontend/src/pages/visit.js
@@ -3,11 +3,13 @@ import Fhxdetail from './Visit/Fhxdetail';
 import ORinfo from './Visit/ORinfo';
 import RTGIinfo from './Visit/RTGIinfo';
 import Symptons from './Visit/Symptons';
+import SearchBar from '../components/SearchBar';
 
 
 const visit = () => (
     <div>
-    
+        
+        <SearchBar />
         <ORinfo />
         <Fhxdetail />
         <RTGIinfo />
