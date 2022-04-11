a018bf3dfbb58864d84e75353ba6ada41ddcfad9
Connor <connor.j.meakin@gmail.com>
2022-04-11 00:37:42 -0600

    Zfu added model identity generation ZfuService null input handling, adjusted update method

--- src/main/java/com/PatManSystem/main/Controllers/ZfuController.java
+++ src/main/java/com/PatManSystem/main/Controllers/ZfuController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/zfu" ,
+@RequestMapping(path = "/api/zfu",
         method = {RequestMethod.GET,
                 RequestMethod.DELETE,
                 RequestMethod.POST})
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("*")
 public class ZfuController {
 
     private final ZfuService zfuService;
