640b2c2a75f8833a1c8bb1e03b305edddb6b7426
Connor <connor.j.meakin@gmail.com>
2022-04-11 00:31:38 -0600

    Typeofrecure added model identity generation TypeofrecureService null input handling, adjusted update method

--- src/main/java/com/PatManSystem/main/Controllers/TypeofrecureController.java
+++ src/main/java/com/PatManSystem/main/Controllers/TypeofrecureController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/typeofrecure" ,
+@RequestMapping(path = "/api/typeofrecure",
         method = {RequestMethod.GET,
                 RequestMethod.DELETE,
                 RequestMethod.POST})
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("*")
 public class TypeofrecureController {
 
     private final TypeofrecureService typeofrecureService;
--- src/main/java/com/PatManSystem/main/Models/Typeofrecure.java
+++ src/main/java/com/PatManSystem/main/Models/Typeofrecure.java
@@ -1,17 +1,21 @@
 package com.PatManSystem.main.Models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.LinkedHashSet;
-import java.util.Set;
+
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "typeofrecure")
 public class Typeofrecure {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "TypeID", nullable = false)
     private Integer id;
 
